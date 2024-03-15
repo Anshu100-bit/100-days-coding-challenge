41b2884b53c2c062dc6ab3a5c905cea3b9df444a
Anshu Kumar <146913693+Anshu100-bit@users.noreply.github.com>
2024-03-15 20:24:19 +0530

    day 18

---
+++ Bit_difference.cpp
@@ -0,0 +1,7 @@
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        return __builtin_popcount(a^b);
+    }
+};

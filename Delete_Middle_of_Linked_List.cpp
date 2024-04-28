8a918ec1b4e5e750a15c92df18da861207bf68cc
Anshu Kumar <146913693+Anshu100-bit@users.noreply.github.com>
2024-04-28 21:35:33 +0530

    day 61

---
+++ Delete_Middle_of_Linked_List.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        // Your Code Here
+        if(head->next==NULL)
+        return NULL;
+        Node *temp=NULL,*slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        temp->next=slow->next;
+        return head;
+    }
+};

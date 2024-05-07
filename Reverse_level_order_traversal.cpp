0c655cec2ab632322f1f48ab513f50802fe37d89
Anshu Kumar <146913693+Anshu100-bit@users.noreply.github.com>
2024-05-07 09:43:08 +0530

    day 70

---
+++ Reverse_level_order_traversal.cpp
@@ -0,0 +1,31 @@
+/*   
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+vector<int> reverseLevelOrder(Node *root)
+{
+    // code here
+    vector<int> res;
+    queue<Node *> q;
+    
+    q.push(root);
+    while(!q.empty()) 
+    {
+        auto p = q.front();
+        q.pop();
+        
+        res.push_back(p->data);
+        
+        if(p->right)
+            q.push(p->right);
+            
+        if(p->left)
+            q.push(p->left);
+    }
+    
+    reverse(res.begin(), res.end());
+    return res;
+}
